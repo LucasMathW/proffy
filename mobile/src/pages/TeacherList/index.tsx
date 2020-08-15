6905a64c7dfe7fd5518c0a0595a04ac9ad979017
Lucas Matheus <lucasag0408@outlook.com>
2020-08-15 18:01:30 -0400

    Adicionando os componentes da aplicação

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { View } from 'react-native'
 import styles from './styles'
+import PageHeader from '../../Components/PageHeader'
 
 function TeacherList(){
-    return <View style={styles.container}/>
-     
+    return(
+        <View style={styles.container}>
+            <PageHeader title="Proffys disponíveis"/>
+        </View>
+    )       
 }
 
 export default TeacherList
--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+ import React from 'react'
 import {Ionicons} from '@expo/vector-icons'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
